02a27176ad76c2865405062ad402a91ac1561418
jul es <julos76890bis@hotmail.fr>
2023-03-07 11:17:17 +0100

    Revert "yo"
    This reverts commit beb60a5b910c63c16cad2dc15867f12f05657263.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HomeComponent} from "./login/home.component";
 import {ServeurComponent} from "./serveur/serveur.component";
-import {HomeComponent} from './login/home.component'
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {ReactiveFormsModule} from "@angular/forms";
 import { SigninComponent } from './signin/signin.component';
-import {TokenInterceptor} from './class/interceptor';
-import {AuthService} from './services/AuthService/auth.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServeurComponent,
-    SigninComponent,
     HomeComponent,
-    TokenInterceptor
+    ServeurComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
--- src/services/login/login.service.ts
+++ src/services/login/login.service.ts
@@ -7,9 +7,7 @@ import { FormGroup } from '@angular/forms';
   providedIn: 'root'
 })
 export class LoginService {
-
   private baseUrl = 'localhost:4200';
-  private userSession: any;
 
   constructor(private http: HttpClient) { }
 
