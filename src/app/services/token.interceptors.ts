4fefcedf779c2849c39656db17a00c430fda8e00
ngatimo <ngatimoaxel@gmail.com>
2023-03-13 23:12:25 +0100

    mis a jour de interceptor

---
+++ src/app/services/token.interceptors.ts
@@ -0,0 +1,37 @@
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { catchError, Observable, throwError } from "rxjs";
+import { AuthService } from "./auth.service";
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+
+  constructor(public authService: AuthService) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    if (this.authService.getJwtToken()) {
+      request = this.addToken(request, this.authService.getJwtToken());
+    }
+        return next.handle(request).pipe(
+            catchError((err) => {
+              if (err instanceof HttpErrorResponse) {
+                  if (err.status === 401) {
+                  //navigate to the login page 
+               }
+            }
+             return throwError(err);
+          })
+         )
+
+  }
+
+  private addToken(request: HttpRequest<any>, token: string) {
+    return request.clone({
+      setHeaders: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+  }
+}
+
