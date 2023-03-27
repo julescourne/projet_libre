b178ed1028f92b4ea5ef3ad916ff2145c6325097
ngatimo <ngatimoaxel@gmail.com>
2023-03-27 13:14:50 +0200

    clean commit

--- src/app/components/create-server-modal/create-server-modal.component.ts
+++ src/app/components/create-server-modal/create-server-modal.component.ts
@@ -1,39 +1,45 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input} from '@angular/core';
 import { FormBuilder, FormControlName, FormGroup } from '@angular/forms';
 import { Virtualemachine } from 'src/app/interface/virtualmachine';
 import {MyAzureVirtualMachineImage } from 'src/app/interface/myAzureVirtualMachineImage'
 import { MyRegions } from 'src/app/interface/regions';
 import {FormControl } from '@angular/forms';
 
-interface Food {
-  value: string;
-  viewValue: string;
-}
 @Component({
   selector: 'app-create-server-modal',
   templateUrl: './create-server-modal.component.html',
   styleUrls: ['./create-server-modal.component.css']
 })
 export class CreateServerModalComponent {
-  regionControl = new FormControl('');
+
+  form = new FormGroup(
+    {
+    vmName : new FormControl(null),  
+
+    region:new FormControl(null),
+  
+    imageDeBase: new FormControl(null),
+  
+    userName:new FormControl(null),
+  
+    password: new FormControl(null),
+  });
+
   hide = true;
-  form!: FormGroup ;
+
   @Input() selectedOption!: string;
-  virtualMachine! : Virtualemachine ; 
   vmImages = Object.values(MyAzureVirtualMachineImage);
   regions = Object.values(MyRegions);
-
-  foods: Food[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'},
-  ];
- 
-
+  
   constructor( private formBuilder : FormBuilder)
 { console.log(this.regions)}
 
   close(){}
 
-  save(){}
+  save(){
+
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+  }
 }
